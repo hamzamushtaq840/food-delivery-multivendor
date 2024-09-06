28bd81996592d7ed7561ac4f9506109d44095afa
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-09-06 16:56:25 +0500

    Update constants.ts

--- enatega-multivendor-web/src/config/constants.ts
+++ enatega-multivendor-web/src/config/constants.ts
@@ -3,5 +3,5 @@ import { Libraries } from '@react-google-maps/api';
 
 //TODO: remove it after the task is checked
 export const GRAPGHQL_URL = 'https://enatega-multivendor.up.railway.app/graphql';
-export const GOOGLE_MAP_API = 'AIzaSyBFl_RbbAYl8WeodWJppLRJOcV1PFXvnxw';
+export const GOOGLE_MAP_API = '';
 export const LIBRARIES: Libraries = ['places', 'drawing', 'geometry', 'visualization'];
